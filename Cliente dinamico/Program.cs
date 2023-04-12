84dd5456282b87744911e6751bc8f7ee8a16a90d
washington1313 <123412352+washington1313@users.noreply.github.com>
2023-04-11 22:28:57 -0300

    Add files via upload

---
+++ Cliente dinamico/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente_dinamico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de Funcionarios a serem cadastrados:");
+            
+            int contador = int.Parse(Console.ReadLine());
+            string [] nomeCliente = new string[contador];
+
+            Console.Clear();
+
+            for (int i = 0; i < nomeCliente.Length; i++) 
+            {
+                if ( i < contador ) 
+                {
+                    Console.WriteLine("Informe o nome para cadastro:");
+                    nomeCliente[i] =Console.ReadLine();
+                    Console.Clear();
+                }                
+            }
+
+            for (int i = 0; i < nomeCliente.Length;i++) 
+            {
+                if (i <= contador)
+                {
+                    Console.WriteLine(nomeCliente[i]);
+                }
+            }
+            Console.ReadKey();            
+        }
+    }
+}
